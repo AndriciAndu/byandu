b9a3773a7186bbba31d050e4ca6d9f414c1bd6a5
Andu <andrici.andu@gmail.com>
2018-12-27 20:38:28 +0200

    asd

--- xFiles/js/route_intermediary.js
+++ xFiles/js/route_intermediary.js
@@ -48,18 +48,7 @@
 		// redirect to index page - (with route details included in the href)
 
 			var redirectUrl = url_indexPage + url_articleString + href_hash + href_search;
-
-			switch(window.location.protocol) {
-				case 'file:' :
-					redirectUrl = url_indexPage + url_articleString + href_hash + href_search; break;
-
-				case 'http:' : 
-				case 'https:': 
-					redirectUrl = url_articleString + href_hash + href_search;
-					break;
-			};
-			console.log(redirectUrl);
-			location.replace(redirectUrl);
+			location.href = redirectUrl;
 
 
 			// var newURL = window.location.protocol + "://" + window.location.host + "/" + window.location.pathname;
